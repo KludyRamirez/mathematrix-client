ebf1a44c0edb386dde3e7f2fd5d2260c5687276c
Kludy Ramirez <kludyaxie6@gmail.com>
2025-03-16 02:44:25 +0800

    fix: leaderboards module

---
+++ src/components/StarsCanvas.js
@@ -0,0 +1,44 @@
+import React, { useState, useRef, Suspense } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { Points, PointMaterial } from "@react-three/drei";
+import * as random from "maath/random";
+
+const StarBackground = (props) => {
+  const ref = useRef(null);
+  const [sphere] = useState(() =>
+    random.inSphere(new Float32Array(3000), { radius: 1.2 })
+  );
+
+  useFrame((_, delta) => {
+    if (ref.current) {
+      ref.current.rotation.x -= delta / 11;
+      ref.current.rotation.y -= delta / 25;
+    }
+  });
+
+  return (
+    <group rotation={[0, 0, Math.PI / 4]}>
+      <Points ref={ref} positions={sphere} stride={3} frustumCulled {...props}>
+        <PointMaterial
+          transparent
+          color="blue"
+          size={0.002}
+          sizeAttenuation={true}
+          depthWrite={false}
+        />
+      </Points>
+    </group>
+  );
+};
+
+const StarsCanvas = () => (
+  <div className="w-full h-auto fixed inset-0 -z-10">
+    <Canvas camera={{ position: [0, 0, 1] }}>
+      <Suspense fallback={null}>
+        <StarBackground />
+      </Suspense>
+    </Canvas>
+  </div>
+);
+
+export default StarsCanvas;
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -6,7 +6,6 @@ import {
   BsBookmarkStar,
   BsClockHistory,
   BsController,
-  BsMegaphoneFill,
   BsTrophy,
 } from "react-icons/bs";
 import controller from "../assets/images/controller.png";
@@ -14,8 +13,11 @@ import folder from "../assets/images/folder.png";
 import trophy from "../assets/images/trophy.png";
 import clock from "../assets/images/clock.png";
 import { Link } from "react-router-dom";
-import { ImBell } from "react-icons/im";
-import logo from "../assets/images/logo.png";
+import StarsCanvas from "../components/StarsCanvas";
+
+import gym from "../assets/images/gym.png";
+import crown from "../assets/images/crown.png";
+import silvertrophy from "../assets/images/silver-trophy.png";
 
 const HomePage = () => {
   const dispatch = useDispatch();
@@ -56,11 +58,12 @@ const HomePage = () => {
 
   return (
     <>
-      <div className="w-full h-screen bg-gradient-to-b from-blue-100 to-blue-50 text-gray-800 flex items-start justify-center">
-        <div className="w-[80%] flex flex-col items-center justify-center">
+      <StarsCanvas />
+      <div className="w-full h-screen flex items-start justify-center px-20">
+        <div className="w-full flex flex-col items-center justify-center">
           <div className="w-full flex justify-between items-center py-20">
             <div className="flex justify-center items-center gap-8">
-              <span className="text-[56px] font-bold text-6xl text-yellow-900 font-[mighty] tracking-wide">
+              <span className="text-[56px] font-bold text-6xl text-gray-900 font-[mighty] tracking-wide">
                 Mathematrix
               </span>
             </div>
@@ -80,7 +83,7 @@ const HomePage = () => {
               </div>
             </div>
           </div>
-          <div className="flex flex-wrap justify-center items-start gap-8">
+          <div className="w-[1336px] flex flex-wrap justify-center items-start gap-8">
             <Link to="/singleplayer">
               <div className="cursor-pointer relative w-[310px] h-[290px] bg-blue-500 rounded-3xl shadow-2xl shadow-sky-500/50 group hover:bg-white/20 hover:shadow-sky-100/50 hover:border-[1px] border-blue-300 transition-all duration-500">
                 <div className="absolute -top-8 w-full 2xl:left-20 left-14 text-white/10 text-[250px] group-hover:text-blue-600 group-hover:rotate-[40deg] group-hover:-translate-x-10 transition-all duration-500 ease-in-out">
@@ -117,34 +120,27 @@ const HomePage = () => {
 
             <div
               className={`${
-                active === "Leaderboards" ? "hidden" : "block"
-              } cursor-pointer relative w-[310px] h-[290px] bg-blue-500 rounded-3xl shadow-2xl shadow-sky-500/50 group hover:bg-white/20 hover:shadow-sky-100/50 hover:border-[1px] border-blue-300 transition-all duration-500`}
+                active === "Leaderboards"
+                  ? "bg-white/20 shadow-sky-100/50 border-[1px] border-blue-300 text-blue-600"
+                  : "bg-blue-500 shadow-sky-500/50"
+              } cursor-pointer relative w-[310px] h-[290px] rounded-3xl shadow-2xl group text-white hover:bg-white/20 hover:shadow-sky-100/50 hover:border-[1px] hover:border-blue-300 transition-all duration-500`}
               onClick={() => setActive("Leaderboards")}
             >
               <div className="absolute -top-8 w-full 2xl:left-20 left-14 text-white/10 text-[240px] group-hover:text-blue-600 group-hover:rotate-[18deg] group-hover:-translate-x-10 transition-all duration-500 ease-in-out">
                 <BsTrophy />
               </div>
 
-              <div className="absolute -top-12 left-16 w-[274px] text-white/10 text-[250px] hidden group-hover:block">
+              <div
+                className={`${
+                  active === "Leaderboards" ? "block" : "hidden"
+                } absolute -top-12 left-16 w-[274px]`}
+              >
                 <img src={trophy} alt="" className="" />
               </div>
 
-              <p className="absolute bottom-4 left-5 w-full text-white text-[38px] font-[vip-regular] group-hover:text-blue-600 transition-colors duration-500">
+              <p className="absolute bottom-4 left-5 w-full text-[38px] font-[vip-regular] group-hover:text-blue-600">
                 Boards
               </p>
-            </div>
-
-            <div
-              className={`${
-                active === "Leaderboards" ? "block" : "hidden"
-              } w-[646px] h-[606px] flex flex-col justify-end items-center`}
-            >
-              <div className="w-full flex flex-col justify-center items-center">
-                <div className="w-[200px] h-[130px] bg-blue-500"></div>
-                <div className="w-full h-[130px] bg-blue-500">
-                  <div className=""></div>
-                </div>
-              </div>
             </div>
 
             <div
@@ -164,54 +160,88 @@ const HomePage = () => {
               </p>
             </div>
           </div>
-
-          <div className="w-full flex justify-start items-center py-20">
-            <div className="flex flex-col gap-8 items-start">
-              {/* <Link to="/admin">
-                <div className="cursor-pointer text-5xl text-gray-800 font-[vip-bold] hover:text-blue-600 skew-y-6 hover:skew-y-0">
-                  Admin
+          <div className="spacer-medium"></div>
+          <div
+            className={`${
+              active === "Leaderboards" ? "block" : "hidden"
+            } w-[1336px] flex flex-col justify-start items-start pt-8 pb-0 px-8 border-t-[1px] border-l-[1px] border-r-[1px] border-blue-300 rounded-tl-3xl rounded-tr-3xl bg-blue-300/10`}
+          >
+            <div className="text-[38px] font-[vip-regular] text-blue-600">
+              Leaderboards
+            </div>
+            <div className="spacer-small"></div>
+            <div className="spacer-medium"></div>
+            <div className="w-full flex flex-col justify-center items-center gap-[60px]">
+              <div className="w-[240px] flex flex-col justify-center items-center">
+                <div className="w-full flex items-center justify-center relative leading-none gap-8">
+                  <span className="whitespace-nowrap font-[semi-bold] text-[90px] text-yellow-500 inline-block">
+                    1st
+                  </span>
+                  <img src={crown} alt="" className="w-[100px] -mt-[12px]" />
+                  <span className="whitespace-nowrap text-center font-[mighty] tracking-wider text-[28px] text-yellow-950 pt-2 inline-block">
+                    James Jameson
+                  </span>
+                  <span className="whitespace-nowrap font-[semi-bold] text-[90px] text-blue-950 inline-block">
+                    1978
+                  </span>
                 </div>
-              </Link> */}
-              <div
-                className="flex gap-8 items-center cursor-pointer text-5xl text-gray-800 font-[vip-bold] hover:text-red-600 skew-y-6 hover:skew-y-0"
-                onClick={handleLogout}
-              >
-                <span>Sign out</span>
               </div>
-            </div>
-          </div>
-          {/* {active === "Lectures" && (
-            <div className="w-[666px] h-[612px] flex flex-wrap justify-start items-start">
-              <span className="text-[32px] text-blue-700 px-4">
-                Lecture Links
-              </span>
-            </div>
-          )}
-          {active === "Leaderboards" && (
-            <div className="w-[666px] h-[612px] flex flex-col justify-start items-start">
-              <span className="text-[32px] text-gray-700">Leaderboards</span>
-              <div className="spacer-small"></div>
-              <div className="w-full h-[600px] bg-gray-700 p-4 rounded-lg shadow-lg">
+              <div className="w-full flex justify-center items-center gap-2">
+                <div className="w-[50%] flex items-center justify-center relative leading-none gap-6">
+                  <span className="whitespace-nowrap font-[semi-bold] text-[48px] text-gray-500 inline-block">
+                    2nd
+                  </span>
+                  <img src={crown} alt="" className="w-[80px] -mt-[12px]" />
+                  <span className="whitespace-nowrap text-center font-[regular] text-[24px] text-yellow-950 pt-2 inline-block">
+                    Veronica Demition
+                  </span>
+                  <span className="whitespace-nowrap font-[semi-bold] text-[48px] text-blue-950 inline-block">
+                    1690
+                  </span>
+                </div>
+                <div className="w-[50%] flex items-center justify-center relative leading-none gap-6">
+                  <span className="whitespace-nowrap font-[semi-bold] text-[48px] text-yellow-800 inline-block">
+                    3rd
+                  </span>
+                  <img src={crown} alt="" className="w-[86px] -mt-[4px]" />
+                  <span className="whitespace-nowrap text-center font-[regular] text-[24px] text-yellow-950 pt-2 inline-block">
+                    Kludy Ramirez
+                  </span>
+                  <span className="whitespace-nowrap font-[semi-bold] text-[48px] text-blue-950 inline-block">
+                    1570
+                  </span>
+                </div>
+              </div>
+              <div className="w-full border-t-[1px] border-l-[1px] border-r-[1px] border-blue-300 rounded-tl-3xl rounded-tr-3xl flex flex-col justify-start items-start overflow-hidden">
                 <table className="w-full text-left">
                   <thead>
-                    <tr className="border-b border-gray-500">
-                      <th className="p-2">Rank</th>
-                      <th className="p-2">Player</th>
-                      <th className="p-2">Correct ✔ </th>
-                      <th className="p-2">Incorrect ❌ </th>
+                    <tr className="bg-white font-[semi-bold] text-[20px]">
+                      <th className="px-6 py-4">Rank</th>
+                      <th className="p-4">Player</th>
+                      <th className="p-4">Correct</th>
+                      <th className="p-4">Wrong</th>
+                      <th className="p-4">MMR</th>
                     </tr>
                   </thead>
                   <tbody>
                     {leaderboard.length > 0 ? (
                       leaderboard.map((player, index) => (
-                        <tr key={player._id} className="border-gray-600">
-                          <td className="p-2">{index + 1}</td>
-                          <td className="p-2">{player._id}</td>
-                          <td className="p-2 text-green-400">
+                        <tr
+                          key={player._id}
+                          className={`text-[18px] ${
+                            index % 2 === 0 ? "bg-transparent" : "bg-white"
+                          }`}
+                        >
+                          <td className="px-6 pt-4 pb-3">{index + 1}</td>
+                          <td className="px-4 py-3">{player._id}</td>
+                          <td className="px-4 py-3 text-blue-600">
                             {player.totalCorrect}
                           </td>
-                          <td className="p-2 text-red-400">
+                          <td className="px-4 py-3 text-red-600">
                             {player.totalIncorrect}
+                          </td>
+                          <td className="px-4 py-3 text-red-600">
+                            {player.mmr}
                           </td>
                         </tr>
                       ))
@@ -219,7 +249,7 @@ const HomePage = () => {
                       <tr>
                         <td
                           colSpan="4"
-                          className="p-2 text-center text-gray-400"
+                          className="px-4 py-3 text-center text-gray-400"
                         >
                           No leaderboard data available.
                         </td>
@@ -229,6 +259,32 @@ const HomePage = () => {
                 </table>
               </div>
             </div>
+          </div>
+
+          {/* <div className="w-full flex justify-start items-center py-20">
+            <div className="flex flex-col gap-8 items-start">
+              <Link to="/admin">
+                <div className="cursor-pointer text-5xl text-gray-800 font-[vip-bold] hover:text-blue-600 skew-y-6 hover:skew-y-0">
+                  Admin
+                </div>
+              </Link>
+              <div
+                className="flex gap-8 items-center cursor-pointer text-5xl text-gray-800 font-[vip-bold] hover:text-red-600 skew-y-6 hover:skew-y-0"
+                onClick={handleLogout}
+              >
+                <span>Sign out</span>
+              </div>
+            </div>
+          </div> */}
+          {/* {active === "Lectures" && (
+            <div className="w-[666px] h-[612px] flex flex-wrap justify-start items-start">
+              <span className="text-[32px] text-blue-700 px-4">
+                Lecture Links
+              </span>
+            </div>
+          )}
+          {active === "Leaderboards" && (
+            
           )}
           {active === "History" && (
             <div className="w-[666px] h-[612px] flex flex-col justify-start items-start">
